51b9d8f0b0b4b6c08ed66136dadc3e5247daa5da
Capricciko <lyricrose.lr@gmail.com>
2026-07-28 21:44:59 -0400

    Player 1.4

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -1,86 +1,86 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-     public InputActionAsset InputActions;
-     public CharacterController playerCharacterController;
+public InputActionAsset InputActions;
+public CharacterController playerCharacterController;
 
-     public InputAction playerMoveAction;
-     public InputAction playerJumpAction;
+[SerializeField] private Transform playerCamera;
 
-     [SerializeField]
-     private Transform playerCamera;
+private InputAction playerMoveAction;
+private InputAction playerJumpAction;
 
-     private Vector2 playerMoveAmount;
+private Vector2 playerMoveAmount;
 
-     private float playerWalkSpeed = 5.0f;
-     private float playerRotateDampening = 0.1f;
-     private float turnSmoothingVelovity;
+private float playerWalkSpeed = 5.0f;
+private float playerRotateDampening = 0.1f;
+private float turnSmoothingVelocity;
 
-     private float verticalVerocity = 0f;
-     private float gravity = -9.8f;
-     private float jumpHeight = 5.0f;
+private float verticalVelocity = 0f;
+private float gravity = -9.81f;
+private float jumpHeight = 5.0f;
 
-     private void OnEnable()
-     {
-         InputActions.FindActionMap("Player").Enable();
-     }
+private void OnEnable()
+{
+InputActions.FindActionMap("Player").Enable();
+}
 
-     private void OnDisable()
-     {
-        InputActions.FindActionMap("player").Disable();
-     }
+private void OnDisable()
+{
+InputActions.FindActionMap("Player").Disable();
+}
 
-     private void Awake()
-     {
-        playerMoveAction = InputSystem.actions.FindAction("Move");
-        playerJumpAction = InputSystem.actions.FindAction("Jump");
-     }
-     
-     private void Update()
-     {
-        playerMoveAmount = playerMoveAction.ReadValue<Vector2>();
-        PlayerMoveAndRotate();
-        Jump();
-     }
+private void Awake()
+{
+playerMoveAction = InputSystem.actions.FindAction("Move");
+playerJumpAction = InputSystem.actions.FindAction("Jump");
+}
 
-     private void PlayerMoveAndRotate()
-     {
-        Vector3 playerDirection = new Vector3(playerMoveAmount.x, 0f, playerMoveAmount.y).normalized;
-        Vector3 verticalMove = new Vector3(0f, verticalVelocity, 0f) * Time.deltaTime;
+private void Update()
+{
+playerMoveAmount = playerMoveAction.ReadValue<Vector2>();
 
-        if(playerDirection.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(playerDirection.x, playerDirection.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
-            float smoothTargetAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothingVelocity, playerRotateDampening);
+Jump();
+PlayerMoveAndRotate();
+}
+private void Jump()
+{
+if (playerCharacterController.isGrounded)
+{
+verticalVelocity = -1f; // Small downward force
 
-            transform.rotation = Quaternion.Euler(0f, smoothTargetAngle, 0f);
+if (playerJumpAction.WasPressedThisFrame())
+{
+verticalVelocity = jumpHeight;
+}
+}
+else
+{
+verticalVelocity += gravity * Time.deltaTime;
+}
+}
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            playerCharacterController.Move(moveDirection.normalized * playerWalkSpeed * turnSmoothingVelovity.deltaTime + verticalMove);
-        }
-        else
-        {
-            playerCharacterController.Move(VerticalMove);
-        }
-     }
+private void PlayerMoveAndRotate()
+{
+Vector3 playerDirection = new Vector3(playerMoveAmount.x, 0f, playerMoveAmount.y).normalized;
+Vector3 verticalMove = new Vector3(0f, verticalVelocity, 0f) * Time.deltaTime;
 
+if (playerDirection.magnitude >= 0.1f)
+{
+float targetAngle = Mathf.Atan2(playerDirection.x, playerDirection.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+float smoothTargetAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothingVelocity, playerRotateDampening);
 
-    private void Jump()
-    {
-        if(playerCharacterController.isGrounded)
-        {
-            verticalVelocity = -1f;
+transform.rotation = Quaternion.Euler(0f, smoothTargetAngle, 0f);
 
-            if(playerJumpAction.WasPressedThisFrame())
-            {
-                verticalVerocity = jumpHeight;
-            }
-        }
-        else
-        {
-            verticalVerocity += gravity * Time.deltaTime;
-        }
-    }
+Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+playerCharacterController.Move(moveDirection.normalized * playerWalkSpeed * Time.deltaTime + verticalMove);
+}
+else
+{
+playerCharacterController.Move(verticalMove);
+}
+}
+
 }
